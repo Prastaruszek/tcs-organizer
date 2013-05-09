d4dc090fc4bed9bd18e409ec11fab0896803fe3c
U-zealot\laiq <laiq@zealot.(none)>
2013-05-09 19:53:19 +0200

    Resizable gui.

--- src/main/java/views/gui/Calendar.java
+++ src/main/java/views/gui/Calendar.java
@@ -64,64 +64,56 @@ public class Calendar {
 		
 		JPanel panel = new JPanel();
 		
+		JPanel panel_2 = new JPanel();
+		
 		table = new JTable();
 		
 		JPanel panel_1 = new JPanel();
-		
-		JPanel panel_2 = new JPanel();
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-							.addComponent(panel, GroupLayout.PREFERRED_SIZE, 350, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 229, GroupLayout.PREFERRED_SIZE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(12)
-							.addComponent(table, GroupLayout.PREFERRED_SIZE, 596, GroupLayout.PREFERRED_SIZE)
-							.addGap(18)
-							.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 152, GroupLayout.PREFERRED_SIZE)))
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 543, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 229, GroupLayout.PREFERRED_SIZE)
+					.addGap(6))
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addComponent(table, GroupLayout.DEFAULT_SIZE, 675, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
+						.addComponent(panel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(panel_2, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+					.addGap(1)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(panel_2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addPreferredGap(ComponentPlacement.RELATED, 22, Short.MAX_VALUE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(panel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)))
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-						.addComponent(panel_1, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(table, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 290, Short.MAX_VALUE))
-					.addContainerGap())
+							.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 314, Short.MAX_VALUE)
+							.addGap(11))
+						.addComponent(table, GroupLayout.DEFAULT_SIZE, 315, Short.MAX_VALUE)))
 		);
+		panel_1.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JButton btnAddEvent = new JButton("Add Event");
+		panel_1.add(btnAddEvent);
+		
+		JButton btnSettings = new JButton("Settings");
+		panel_1.add(btnSettings);
+		
+		JButton btnImport = new JButton("Import");
+		panel_1.add(btnImport);
+		
+		JButton btnExport = new JButton("Export");
+		panel_1.add(btnExport);
 		
 		JLabel lblVelocity = new JLabel("Velocity: 9000");
 		lblVelocity.setFont(new Font("Dialog", Font.BOLD, 20));
 		panel_2.add(lblVelocity);
-		
-		JButton btnNewButton = new JButton("Import");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		
-		JButton btnAddEvent = new JButton("Add Event");
-		
-		JButton btnNewButton_1 = new JButton("Export");
-		
-		JButton btnSettings = new JButton("Settings");
-		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		panel_1.add(btnAddEvent);
-		panel_1.add(btnNewButton_1);
-		panel_1.add(btnNewButton);
-		panel_1.add(btnSettings);
 		
 		JButton btnPrevious = new JButton("Previous");
 		panel.add(btnPrevious);
