5b2824d020641bfb51b17dc92ffac8c0ce7fe440
n3v <thezbylut@gmail.com>
2013-05-21 18:54:20 +0200

    Settings now properly update velocity in Calendar label

--- src/main/java/controllers/SettingsController.java
+++ src/main/java/controllers/SettingsController.java
@@ -15,7 +15,9 @@ public class SettingsController extends Controller {
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		Organizer.getInstance().getCurrentUser().getUserProfile().setPath(ref.getPath());
-		Organizer.getInstance().getCurrentUser().getUserProfile().setVelocity(ref.getVelocity());
+			Organizer.getInstance().getCurrentUser().getUserProfile().setVelocity(ref.getVelocity());
+		Organizer.getInstance().update();
+		Organizer.getInstance().notifyObservers();
 		ref.dispose();
 	}
 	
