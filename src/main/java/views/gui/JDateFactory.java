20903266342b0d5f034f25d23be65cf22a31f0a9
laiqu <laiquendir@gmail.com>
2013-05-23 22:54:16 +0200

    mistar wanted some changes.W

--- src/main/java/views/gui/JDateFactory.java
+++ src/main/java/views/gui/JDateFactory.java
@@ -10,13 +10,13 @@ public final class JDateFactory {
 	/**
 	 * @wbp.factory
 	 */
-	public static JTextPane JDate() {
+	public static JTextPane JDate(java.util.Calendar calendar) {
 		JTextPane textPane = new JTextPane();
 		textPane.setEditable(false);
 		textPane.setBackground(UIManager.getColor("Button.background"));
 		textPane.setFont(new Font("Tahoma", Font.PLAIN, 9));
 		textPane.setContentType("text/html");
-		textPane.setText(DatePickerController.dateDisplay(GregorianCalendar.getInstance()));
+		textPane.setText(DatePickerController.dateDisplay(calendar));
 		textPane.insertIcon(new ImageIcon(Calendar.SRC_MAIN_IMAGES_DATE_PICKER_ICON_GIF));
 		return textPane;
 	}
