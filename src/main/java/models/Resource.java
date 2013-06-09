a593323419fbab86f4c02e49a105090b5ccf6a7f
Michał Dyrek <butozerca@gmail.com>
2013-06-09 22:44:12 +0200

    javadoc addstrategy

--- src/main/java/models/Resource.java
+++ src/main/java/models/Resource.java
@@ -1,12 +1,5 @@
 package models;
 
-/**
- * Created with IntelliJ IDEA.
- * User: laiq
- * Date: 5/28/13
- * Time: 12:40 AM
- * To change this template use File | Settings | File Templates.
- */
 public interface Resource {
     void open();
 }
