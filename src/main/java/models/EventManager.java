f4046d508b6b178be9b171e869d2d812940554b9
patryku <patryk.u93@gmail.com>
2013-06-10 00:16:49 +0200

    javadoc: EventManager

--- src/main/java/models/EventManager.java
+++ src/main/java/models/EventManager.java
@@ -5,31 +5,54 @@ import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.util.Calendar;
 
+/** This class provides methods for event management.
+ * Every instance of this class contains a reference to the associated set of events.
+ */
 public class EventManager implements Serializable{
 	
 	private static final long serialVersionUID = -2897014193380647665L;
 	private final EventSet events;
 
+	/** Constructs a new event manager with an empty event set.
+     */
     public EventManager() {
         this.events = new EventSet(this);
     }
 
+    /** Creates and returns new event set containing all events in the associated event set.
+     * @see EventSet#all()
+     */
     public EventSet all() {
         return events.all();
     }
 
+    /** Returns an EventSet of Events that are between startTime and endTime.
+     * @see EventSet#between(Calendar, Calendar)
+     */
     public EventSet between(Calendar startTime, Calendar endTime) {
         return events.between(startTime, endTime);
     }
 
+    /** Returns a subset of the associated EventSet that overlaps with whole-day interval given by parameters.
+     * @see EventSet#overlapping(Calendar, Calendar)
+     */
     public EventSet overlapping(Calendar startTime, Calendar endTime) {
         return events.overlapping(startTime, endTime);
     }
 
+    /** Adds a given event to the associated event set.
+     * @param e the event to be added.
+     * @return true if the set did not already contain the specified event.
+     * @see EventSet#add(Event)
+     */
     public boolean add(Event e) {
         return events.add(e);
     }
 
+    /** Removes a given event from the associated event set.
+     * @param e the event to be removed.
+     * @see EventSet#remove(Object)
+     */
 	public void removeEvent(Event e) {
 		events.remove(e);
 	}
@@ -63,10 +86,14 @@ public class EventManager implements Serializable{
 		return leftovers;
 	}
 	
-	public static EventSet importEventSet(String filename){
+	/** Imports event set from file with a given path.
+	 * @param pathname the given path.
+	 * @return imported event set if reading was successful, null otherwise.
+	 */
+	public static EventSet importEventSet(String pathname){
     	EventSet res = null;
     	try {
-			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename));
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(pathname));
 			res = (EventSet) ois.readObject();
 			ois.close();
 		} catch (Exception e) {
@@ -75,6 +102,9 @@ public class EventManager implements Serializable{
     	return res;
     }
    
+	/** Returns string representation of this event manager.
+     * @return string representation of this event manager.
+     */
     public String toString(){
     	String res = "EventMananger[";
     	res += events;
