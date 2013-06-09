c2667ffe701ef37ad6491e3c76c669049eb8438a
patryku <patryk.u93@gmail.com>
2013-06-09 21:46:01 +0200

    javadoc comments: EventPriority

--- src/main/java/models/EventPriority.java
+++ src/main/java/models/EventPriority.java
@@ -3,6 +3,10 @@ package models;
 import java.awt.Color;
 import java.io.Serializable;
 
+/** This class contains constant enum values representing event priorities.
+ * There are five event priorities numbered from 1 (most important) to 5 (least important).
+ * Each priority defines a color which is used for displaying event in graphical user interface.
+ */
 public enum EventPriority implements Serializable {
 	
 	ONE(1, "I"), TWO(2, "II"), THREE(3, "III"), FOUR(4, "IV"), FIVE(5, "V");
@@ -17,6 +21,10 @@ public enum EventPriority implements Serializable {
 		this.roman = roman;
 	}
 	
+	/** Returns proper object representing given priority.
+	 * @param priority priority number, must be in [1,5].
+	 * @return object representing given priority.
+	 */
 	public static EventPriority getEnum(int priority) {
 		switch(priority) {
 			case 1: return ONE;
@@ -27,18 +35,33 @@ public enum EventPriority implements Serializable {
 		}
 	}
 	
+	/** Returns priority value of this event priority.
+     * @return priority value of this event priority.
+     */
 	public int getPriority() {
 		return priority;
 	}
 	
+	/** Returns priority value of this event priority in Roman notation.
+     * @return priority value of this event priority in Roman notation.
+     */
 	public String getRomanPriority() {
 		return roman;
 	}
 	
+	/** Returns default color of this event priority.
+     * @return priority value of this event priority.
+     */
 	public Color getDefaultColor(int priority) {
 		return defaultColor[priority-1];
 	}
 	
+	/** Returns color of this event priority for a given user profile.
+	 * If a custom color for the user is defined, it will be used. 
+	 * Otherwise the default color will be returned.
+	 * @param profile the user profile
+     * @return color of this event priority for the given user profile.
+     */
 	public Color getColor(UserProfile profile) {
 		Color custom = profile.getPriorityColor(priority);
 		if(custom != null)
@@ -46,6 +69,9 @@ public enum EventPriority implements Serializable {
 		return getDefaultColor(priority);
 	}
 	
+	/** Returns priority value of this event priority in Roman notation.
+     * @return priority value of this event priority in Roman notation.
+     */
 	@Override
 	public String toString() {
 		return roman;
