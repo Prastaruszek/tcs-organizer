c4ce06c404fbe82e91870fcc47b3e4172fecde30
Michał Dyrek <butozerca@gmail.com>
2013-06-09 21:12:19 +0200

    AddStrategyController

---
+++ src/main/java/controllers/AddStrategyController.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import java.awt.event.ActionEvent;
+import java.util.Calendar;
+
+import models.AddStrategy;
+import models.Event;
+import models.Task;
+import models.User;
+
+public class AddStrategyController extends Controller {
+	
+	private AddStrategy addStrategy;
+	private Event event;
+	private User currentUser;
+	private Calendar time;
+	
+	public AddStrategyController(AddStrategy addStrategy, Event event, User currentUser, Calendar time) {
+		this.addStrategy = addStrategy;
+		this.event = event;
+		this.currentUser = currentUser;
+		this.time = time;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent X) {
+		Event newEvent = addStrategy
+				.getAddableEvent(new Task(event), currentUser.getUserProfile().getEvents(), time);
+		newEvent.setProfile(currentUser.getUserProfile());
+		
+		currentUser.getUserProfile().getEvents().add(newEvent);
+	}
+
+}
