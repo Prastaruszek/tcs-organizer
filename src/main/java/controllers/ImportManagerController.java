807394276a0f3432b74603871390dc8232349fa1
Michał Dyrek <butozerca@gmail.com>
2013-06-10 15:57:21 +0200

    impot manager setup

---
+++ src/main/java/controllers/ImportManagerController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import models.Event;
+import models.EventSet;
+import views.gui.components.JEventBox;
+
+public class ImportManagerController extends Controller {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	}
+	
+}
--- src/main/java/views/gui/ImportManager.java
+++ src/main/java/views/gui/ImportManager.java
@@ -2,16 +2,63 @@ package views.gui;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import models.EventSet;
+
 import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.BoxLayout;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class ImportManager extends JFrame {
-	public ImportManager() {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ImportManager frame = new ImportManager(new EventSet());
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	private static final long serialVersionUID = -328398734659873L;
+	private JTextField txtTip;
+
+	public ImportManager(EventSet leftover) {
 		
-		JPanel panel = new JPanel();
-		getContentPane().add(panel, BorderLayout.NORTH);
+		JPanel SearchField = new JPanel();
+		getContentPane().add(SearchField, BorderLayout.NORTH);
 		
-		JPanel panel_1 = new JPanel();
-		getContentPane().add(panel_1, BorderLayout.SOUTH);
+		JPanel BotBtns = new JPanel();
+		getContentPane().add(BotBtns, BorderLayout.SOUTH);
+		
+		JPanel EventView = new JPanel();
+		getContentPane().add(EventView, BorderLayout.CENTER);
+		EventView.setLayout(new BoxLayout(EventView, BoxLayout.Y_AXIS));
+		 
+		
+		JButton btnOK = new JButton("OK");
+		BotBtns.add(btnOK);
+		
+		JButton Cancel = new JButton("Cancel");
+		Cancel.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+		});
+		BotBtns.add(Cancel);
+		
+		
 	}
 
 }
